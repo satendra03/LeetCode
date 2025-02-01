c25bd5dbe02097eb45eb1edd79d9e6f2106e1a04
Satendra Kumar Parteti <143379098+satendra03@users.noreply.github.com>
2025-02-01 18:25:34 +0530

    Time: 7 ms (36.89%) | Memory: 20.8 MB (5.75%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
+        vector<unordered_set<int>> toDoThisIhaveToCompleteThese(n);
+        vector<int> indegree(n);
+        for (auto pre : prerequisites) {
+            int a = pre[0];
+            int b = pre[1];
+            indegree[a]++;
+            toDoThisIhaveToCompleteThese[b].insert(a);
+        }
+
+        queue<int> q;
+        vector<int> ans;
+        for(int i=0; i<n; i++) if(!indegree[i]) q.push(i);
+
+        if(q.empty()) return {};
+        
+        while(!q.empty()){
+            int node  = q.front();
+            q.pop();
+            ans.push_back(node);
+            indegree[node] = -1;
+            for(auto i:toDoThisIhaveToCompleteThese[node]){
+                if(indegree[i] > 0) indegree[i]--;
+                if(!indegree[i]) q.push(i);
+            }
+        }
+        if( ans.size() == n ) return ans;
+        return {};
+    }
+};
