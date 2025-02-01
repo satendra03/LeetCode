158daddeea9c46ab0d981a650aae744103b74efa
Satendra Kumar Parteti <143379098+satendra03@users.noreply.github.com>
2025-02-01 18:25:25 +0530

    Time: 16 ms (10.69%) | Memory: 23.4 MB (5.04%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -2,58 +2,30 @@ class Solution {
 public:
     bool canFinish(int n, vector<vector<int>>& prerequisites) {
         vector<unordered_set<int>> toDoThisIhaveToCompleteThese(n);
-        vector<unordered_set<int>> canCompleteTheseIfDoneThis(n);
-        for(auto pre:prerequisites){
+        vector<int> indegree(n);
+        for (auto pre : prerequisites) {
             int a = pre[0];
             int b = pre[1];
+            indegree[a]++;
             toDoThisIhaveToCompleteThese[b].insert(a);
-            canCompleteTheseIfDoneThis[a].insert(b);
         }
 
-        // vector<int>freeCourses;
-        // for(int i=0; i<n; i++){
-        //     if(toDoThisIhaveToCompleteThese[i].size() == 0) freeCourses.push_back(i);
-        // }
+        queue<int> q;
+        vector<int> ans;
+        for(int i=0; i<n; i++) if(!indegree[i]) q.push(i);
 
-        // if(!freeCourses.size()) return false;
+        if(q.empty()) return false;
 
-        vector<bool> completed(n);
-        int ans = true;
-        for(int i=0; i<n; i++){
-            if(toDoThisIhaveToCompleteThese[i].size() == 0){
-                int course = i;
-                if(!completed[course]){
-                    unordered_set<int> prevCourse;
-                    ans = ans and explore(course,canCompleteTheseIfDoneThis,completed,prevCourse);
-                }
+        while(!q.empty()){
+            int node  = q.front();
+            q.pop();
+            ans.push_back(node);
+            indegree[node] = -1;
+            for(auto i:toDoThisIhaveToCompleteThese[node]){
+                if(indegree[i] > 0) indegree[i]--;
+                if(!indegree[i]) q.push(i);
             }
         }
-
-        if(!ans) return ans;
-
-        bool allCompleted = true;
-        for(auto isCompleted:completed) if(!isCompleted) allCompleted = false;
-        return allCompleted;
-    }
-private:
-    // void printSet(unordered_set<int> & a){
-    //     for(auto i:a) cout << i << " ";
-    // }
-    // void printArr(vector<int>& a){
-    //     for(auto i:a) cout << i << " ";
-    // }
-    bool explore(int course,vector<unordered_set<int>>& canCompleteTheseIfDoneThis,
-        vector<bool>& completed,unordered_set<int>&prevCourse
-    ){
-        completed[course] = true;
-        prevCourse.insert(course);
-        bool ans = true;
-        for(auto nextCourse:canCompleteTheseIfDoneThis[course]){
-            if(completed[nextCourse] and prevCourse.find(nextCourse)!=prevCourse.end()) return false;
-            else if(!completed[nextCourse])
-                ans = ans and explore(nextCourse,canCompleteTheseIfDoneThis,completed,prevCourse);
-        }
-        prevCourse.erase(course);
-        return ans;
+        return ans.size() == n;
     }
 };
