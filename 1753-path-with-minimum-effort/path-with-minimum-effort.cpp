e5528e9a71e401e464733f86fe433447ed1366af
Satendra Kumar Parteti <143379098+satendra03@users.noreply.github.com>
2025-03-04 19:00:55 +0530

    Time: 70 ms (36.22%) | Memory: 25.5 MB (35.92%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,44 +1,39 @@
 class Solution {
 public:
-    int n, m;
     int dx[4] = {0, 1, 0, -1};
     int dy[4] = {1, 0, -1, 0};
-
     int minimumEffortPath(vector<vector<int>>& heights) {
-        n = heights.size();
-        m = heights[0].size();
-        int ans = INT_MAX;
-        set<pair<int, pair<int, int>>> pq;
-            //{ diff ,  {x,y} }
-
+        int n = heights.size(), m = heights[0].size();
         vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
+        using T = tuple<int, int, int>;
+        priority_queue<T, vector<T>, greater<T>> pq;
         dist[0][0] = 0;
-        pq.insert({0, {0,0}});
+        pq.push({0,0,0});
         while(!pq.empty()){
-            auto it = pq.begin();
-            pair<int, pair<int, int>> data = *it;
-            pq.erase(it);
-            int diff = data.first;
-            int x = data.second.first;
-            int y = data.second.second;
+            auto [maxEffortOfRoute, x, y] = pq.top();
+            pq.pop();
 
-            if(x==n-1 and y==m-1) return diff;
-            
             for(int i=0; i<4; i++){
                 int newX = x + dx[i];
                 int newY = y + dy[i];
                 if(newX>=0 and newX<n and newY>=0 and newY<m){
-                    int newDiff = max(
-                        abs(heights[newX][newY] - heights[x][y]),
-                        diff
-                    );
-                    if(newDiff < dist[newX][newY]){
-                        dist[newX][newY] = newDiff;
-                        pq.insert({newDiff, {newX, newY}});
+                    // this is the effort that I have to do to clim to this newX, newY
+                    int effortForThisHeight = abs(heights[x][y] - heights[newX][newY]);
+
+                    // if this Effort is the max of the effort till now, for the route then we will take this 
+                    // or else we will continue with the max effort 
+                    // in short we will take the max effort of route ()
+                    int newMaxEffortOfRoute = max(effortForThisHeight, maxEffortOfRoute);
+
+                    // if this new Effort is less than the current effort for this coordinate then
+                    // we will consider this
+                    if(newMaxEffortOfRoute < dist[newX][newY]){
+                        dist[newX][newY] = newMaxEffortOfRoute;
+                        pq.push({newMaxEffortOfRoute, newX, newY});
                     }
                 }
             }
         }
-        return 0;
+        return dist[n-1][m-1];
     }
 };
