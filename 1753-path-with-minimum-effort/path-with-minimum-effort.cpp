5c88ae19908c6a6845aa60ec25c49925060d5008
Satendra Kumar Parteti <143379098+satendra03@users.noreply.github.com>
2025-03-04 22:42:33 +0530

    Time: 236 ms (10.85%) | Memory: 55 MB (12.43%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -4,36 +4,45 @@ public:
     int dy[4] = {1, 0, -1, 0};
     int minimumEffortPath(vector<vector<int>>& heights) {
         int n = heights.size(), m = heights[0].size();
-        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
-        using T = tuple<int, int, int>;
-        priority_queue<T, vector<T>, greater<T>> pq;
-        dist[0][0] = 0;
-        pq.push({0,0,0});
-        while(!pq.empty()){
-            auto [maxEffortOfRoute, x, y] = pq.top();
-            pq.pop();
+        int l = 0, r = 1e6, ans = r;
 
-            for(int i=0; i<4; i++){
-                int newX = x + dx[i];
-                int newY = y + dy[i];
-                if(newX>=0 and newX<n and newY>=0 and newY<m){
-                    // this is the effort that I have to do to clim to this newX, newY
-                    int effortForThisHeight = abs(heights[x][y] - heights[newX][newY]);
+        auto isPath = [&](int limitEffort){
+            using T = tuple<int,int>;
+            queue<T> q;
+            vector<vector<bool>> visited(n, vector<bool>(m, false));
+            q.push({0,0});
+            visited[0][0] = true;
 
-                    // if this Effort is the max of the effort till now, for the route then we will take this 
-                    // or else we will continue with the max effort 
-                    // in short we will take the max effort of route ()
-                    int newMaxEffortOfRoute = max(effortForThisHeight, maxEffortOfRoute);
+            while(!q.empty()){
+                auto [x,y] = q.front();
+                q.pop();
 
-                    // if this new Effort is less than the current effort for this coordinate then
-                    // we will consider this
-                    if(newMaxEffortOfRoute < dist[newX][newY]){
-                        dist[newX][newY] = newMaxEffortOfRoute;
-                        pq.push({newMaxEffortOfRoute, newX, newY});
+                if(x==n-1 and y==m-1) return true;
+
+                for(int i=0; i<4; i++){
+                    int newX = x + dx[i];
+                    int newY = y + dy[i];
+                    if(newX>=0 and newX<n and newY>=0 and newY<m){
+                        int effort = abs(heights[x][y] - heights[newX][newY]);
+                        if(effort <= limitEffort and !visited[newX][newY]){
+                            visited[newX][newY] = true;
+                            q.push({newX, newY});
+                        }
                     }
                 }
             }
+
+            return false;    
+        };
+
+        while(l <= r){
+            int mid = (l+r) / 2;
+            if(isPath(mid)){
+                ans = mid;
+                r = mid - 1;
+            } else l = mid + 1;
         }
-        return dist[n-1][m-1];
+
+        return ans;
     }
 };
