494a3a6dd9561973e58d16ae6ee1bbd35a9ff4a2
Satendra Kumar Parteti <143379098+satendra03@users.noreply.github.com>
2025-05-28 23:45:28 +0530

    Time: 211 ms (54.84%) | Memory: 63.3 MB (61.75%) - LeetSync

--- 3633-maximize-the-number-of-target-nodes-after-connecting-trees-i/maximize-the-number-of-target-nodes-after-connecting-trees-i.cpp
+++ 3633-maximize-the-number-of-target-nodes-after-connecting-trees-i/maximize-the-number-of-target-nodes-after-connecting-trees-i.cpp
@@ -1,21 +1,7 @@
 class Solution {
 public:
-    vector<int> maxTargetNodes(vector<vector<int>>& edges1,
-                               vector<vector<int>>& edges2, int k) {
-        // int n = edges1.size(), m = edges2.size();
-
-        // int maxNode = 0;
-        // for (auto& edge : edges2) {
-        //     maxNode = max({maxNode, edge[0], edge[1]});
-        // }
-        // maxNode++; // because nodes start from 0
-        int n = 0, m = 0;
-
-        for(auto& edge:edges1) n=max({n, edge[0], edge[1]});
-        n++;
-
-        for(auto& edge : edges2) m=max({m, edge[0], edge[1]});
-        m++;
+    vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2, int k) {
+        int n=edges1.size()+1, m=edges2.size()+1;
 
         vector<vector<int>> graph(n);
         vector<vector<int>> adj(m);
@@ -41,7 +27,6 @@ public:
         }
         int val = *max_element(distM.begin(), distM.end());
 
-
         vector<int> answer(n, 0);
         for (int i=0; i<n;i++) {
             for(int j=0; j<m; j++){
@@ -52,18 +37,14 @@ public:
     }
 
 private:
-    int getNodes(vector<vector<int>>& adj, int node, vector<bool>& visited,
-                 int k) {
-        if (visited[node])
-            return 0;
-        if (k < 0)
-            return 0;
+    int getNodes(vector<vector<int>>& adj, int node, vector<bool>& visited, int k) {
+        if (visited[node]) return 0;
+        if (k < 0) return 0;
         visited[node] = true;
-        int ans = 0;
-        for (auto& n : adj[node]) {
-            if (!visited[n])
-                ans += getNodes(adj, n, visited, k - 1);
+        int ans=0;
+        for(auto& n:adj[node]) {
+            if (!visited[n]) ans+=getNodes(adj, n, visited, k - 1);
         }
-        return ans + 1;
+        return ans+1;
     }
 };
