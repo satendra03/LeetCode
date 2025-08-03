5d7e0a6e3bc240d9c40d7833816277ae6977e06c
Satendra Kumar Parteti <143379098+satendra03@users.noreply.github.com>
2025-08-03 17:36:41 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (46.39%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -4,8 +4,20 @@ public:
         int n = nums.size();
         if(n <= 1) return nums[0];
 
-        dp.resize(n, -1);
-        return f(0, nums);
+        // dp.resize(n, -1);
+        // return f(0, nums);
+
+        vector<int> dp(n, 0);
+        dp[0] = nums[0];
+        for(int i=1; i<n; i++){
+            int rob = nums[i], skip = 0;
+            if(i-2 >=0) rob += dp[i-2];
+            if(i-1 >=0) skip = dp[i-1];
+            dp[i] = max(rob, skip);
+
+
+        }
+        return dp[n-1];
     }
 private:
     vector<int> dp;
@@ -15,9 +27,9 @@ private:
 
         if(dp[i] != -1) return dp[i];
         
-        int rob_current = nums[i] + f(i + 2, nums);
-        int skip_current = f(i + 1, nums);
+        int rob = nums[i] + f(i + 2, nums);
+        int skip = f(i + 1, nums);
 
-        return dp[i] = max(rob_current, skip_current);
+        return dp[i] = max(rob, skip);
     }
 };
