79841a658e49464d3ecb1e969a35d0ef7b5d1f86
Satendra Kumar Parteti <143379098+satendra03@users.noreply.github.com>
2025-08-03 16:35:45 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (22.55%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -5,7 +5,7 @@ public:
         if(n <= 1) return nums[0];
 
         dp.resize(n, -1);
-        return max(nums[0]+f(0, nums), nums[1]+f(1, nums) );
+        return f(0, nums);
     }
 private:
     vector<int> dp;
@@ -15,15 +15,9 @@ private:
 
         if(dp[i] != -1) return dp[i];
         
-        int money = 0;
-        for(int index=i+2; index<n; index++){
-            int newMoney = INT_MIN;
-            if(index < n){
-                newMoney = nums[index] + f(index, nums);
-            }
-            money = max(money, newMoney);
-        }
+        int rob_current = nums[i] + f(i + 2, nums);
+        int skip_current = f(i + 1, nums);
 
-        return dp[i] = money;
+        return dp[i] = max(rob_current, skip_current);
     }
 };
