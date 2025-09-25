565fff2c3b3265e6d5dcda8eab1025a293d1c4c6
Satendra Kumar Parteti <143379098+satendra03@users.noreply.github.com>
2025-09-25 16:14:22 +0530

    Time: 3 ms (27.41%) | Memory: 97.4 MB (60.67%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -2,22 +2,12 @@ class Solution {
 public:
     int maxProfit(vector<int>& nums) {
         int n = nums.size();
-        if(n==1) return 0;
-        vector<int> nextGreat(n, 0);
-        nextGreat[n-1] = -1;
-        for(int i=n-2; i>=0; i--){
-            if(nextGreat[i+1] == -1){
-                if(nums[i+1] > nums[i]) nextGreat[i] = nums[i+1];
-                else nextGreat[i] = -1;
-            } else {
-                if(nextGreat[i+1] > nums[i]) nextGreat[i] = nextGreat[i+1];
-                else nextGreat[i] = -1;
-            }
+        int profit = 0;
+        int minPrice = nums[0];
+        for(int i=1; i<n; i++){
+            profit = max(profit, nums[i] - minPrice);
+            minPrice = min(minPrice, nums[i]);
         }
-        int ans = 0;
-        for(int i=0; i<n; i++){
-            ans = max(ans, nextGreat[i]-nums[i]);
-        }
-        return ans;
+        return profit;
     }
 };
