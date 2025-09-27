741145b93d2b71e9973dacc720b071a3170a911a
Satendra Kumar Parteti <143379098+satendra03@users.noreply.github.com>
2025-09-28 02:04:25 +0530

    Time: 1 ms (25.31%) | Memory: 27.3 MB (30.81%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -4,16 +4,21 @@ public:
         int n = arr.size();
         vector<int> preMax(n, -1), nextMax(n, -1);
 
-        for(int i=n-2; i>=0; i--){
-            if(nextMax[i+1] == -1) {
-                if(arr[i+1] > arr[i]) nextMax[i] = arr[i+1];
-            } else if(nextMax[i+1] > arr[i]) nextMax[i] = nextMax[i+1]; 
-        }
-        for(int i=1; i<n; i++){
-            if(preMax[i-1] == -1) {
-                if(arr[i-1] > arr[i]) preMax[i] = arr[i-1];
-            } else if(preMax[i-1] > arr[i]) preMax[i] = preMax[i-1];
-        }
+        // for(int i=n-2; i>=0; i--){
+        //     if(nextMax[i+1] == -1) {
+        //         if(arr[i+1] > arr[i]) nextMax[i] = arr[i+1];
+        //     } else if(nextMax[i+1] > arr[i]) nextMax[i] = nextMax[i+1]; 
+        // }
+        // for(int i=1; i<n; i++){
+        //     if(preMax[i-1] == -1) {
+        //         if(arr[i-1] > arr[i]) preMax[i] = arr[i-1];
+        //     } else if(preMax[i-1] > arr[i]) preMax[i] = preMax[i-1];
+        // }
+
+        preMax[0] = arr[0];
+        for(int i=1; i<n; i++) preMax[i] = max(arr[i], preMax[i-1]);
+        nextMax[n-1] = arr[n-1];
+        for(int i=n-2; i>=0; i--) nextMax[i] = max(arr[i], nextMax[i+1]);
 
         int ans = 0;
         for(int i=0; i<n; i++){
