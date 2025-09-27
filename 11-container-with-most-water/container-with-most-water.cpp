d2e54b433485b375be0ed91d1113c70c7bc39058
Satendra Kumar Parteti <143379098+satendra03@users.noreply.github.com>
2025-09-28 00:39:08 +0530

    Time: 0 ms (100.00%) | Memory: 62.9 MB (48.00%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n = height.size();
+        int ans = 0;
+        int left = 0, right = n-1; // pointer to left and right most
+
+        while(left < right){
+            int distance = right - left; // lenght of the rectangle
+            int Height = min(height[left], height[right]); // breadth of the rectangle
+
+            ans = max(ans, distance*Height);
+            // lenght * breadth -> this would store the max area or we can say water, so we will just max the area
+            // now only change the length and breadth when we find the max l or b
+
+            // we'll find the next greater height again calculate the area
+            if(height[left] < height[right]){
+                int leftPtr = left+1;
+                for(; leftPtr<right; leftPtr++) if(height[leftPtr] > height[left]) break;
+                left = leftPtr;
+            } else {
+                int rightPtr = right-1;
+                for(; rightPtr>left; rightPtr--) if(height[rightPtr] > height[right]) break;
+                right = rightPtr;
+            }
+
+        }
+        return ans;
+
+    }
+};
